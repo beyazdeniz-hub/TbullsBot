bb51bf9ecc60fea2e6de37fb7eb5ee86979d2af7
beyazdeniz-hub <beyazdeniz@gmail.com>
2026-04-14 00:43:52 +0300

    Refactor test.js to log and exit immediately
    Removed axios function call and added console log.

--- test.js
+++ test.js
@@ -1,19 +1,2 @@
-const axios = require("axios");
-
-async function test() {
-  try {
-    const res = await axios.get("https://www.alphavantage.co/query", {
-      params: {
-        function: "TIME_SERIES_DAILY",
-        symbol: "THYAO.IST",
-        apikey: "T3DAES5QSOIM2T40",
-        outputsize: "compact"
-      }
-    });
-    console.log(JSON.stringify(res.data).substring(0, 500));
-  } catch (e) {
-    console.log("Hata:", e.message);
-  }
-}
-
-test();
+console.log("BASLADI");
+process.exit(0);
