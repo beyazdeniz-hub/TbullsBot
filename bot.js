62d0ed843e03ac59b269097a99f2029f58e8da31
beyazdeniz-hub <beyazdeniz@gmail.com>
2026-04-14 00:41:24 +0300

    Refactor bot.js to remove axios calls
    Removed axios requests and added exit command.

--- bot.js
+++ bot.js
@@ -1,23 +1,2 @@
-const axios = require("axios");
-
-async function test() {
-  try {
-    const res = await axios.get("https://httpbin.org/get", { timeout: 10000 });
-    console.log("Internet erisimi VAR");
-  } catch (e) {
-    console.log("Internet erisimi YOK:", e.message);
-  }
-
-  try {
-    const res = await axios.get("https://query1.finance.yahoo.com/v8/finance/chart/THYAO.IS", {
-      params: { interval: "1d", range: "3mo" },
-      headers: { "User-Agent": "Mozilla/5.0" },
-      timeout: 15000
-    });
-    console.log("Yahoo:", JSON.stringify(res.data).substring(0, 300));
-  } catch (e) {
-    console.log("Yahoo hatasi:", e.response?.status, e.message);
-  }
-}
-
-test();
+console.log("BASLADI");
+process.exit(0);
